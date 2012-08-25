eda784b4a6e214ec39212ed1361767eb73a097c4
William Roe <willroe@gmail.com>
2012-08-25 15:58:21 +0100

    Diggy diggy hole!

--- src/cljs/chaotic_god/game.cljs
+++ src/cljs/chaotic_god/game.cljs
@@ -5,8 +5,33 @@
             [goog.events.EventType :as event-type]
             [goog.events.KeyCodes :as key-codes]))
 
-(def sq-width 30)
-(def sq-height 30)
+;; Canvas size should fit the world and pixels below
+(def sq-width 50)
+(def sq-height 50)
+
+(def initial-world
+  [" M             "
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"
+   "ddddddddddddddd"])
+
+(def tile-to-colour
+  {\M [67 67 67]
+   \space [124 122 255]
+   \d [87 53 1]
+   \t [127 128 106]})
+
+(def tile-to-meaning
+  {\M :mine
+   \t :tunnel
+   \d :dirt
+   \space :sky})
 
 (defn grid-to-dimentions
   "Take grid coords, [0 0], [0 2] and make coords out of them for the squares"
@@ -41,11 +66,60 @@
     (. surface (closePath))
     (. surface (fill))))
 
+(defn draw-square
+  [surface x y tile]
+  (fill-rect surface [x y sq-width sq-height] (tile tile-to-colour [0 0 0])))
+
 (defn update-canvas
-  [world surface]
-  (let [[_ width height] surface]
-    (fill-rect surface [0 0 width height] [10 10 10])
-    (stroke-rect surface [0 0 width height] 2 [0 0 0])))
+  [state surface]
+  (let [[_ width height] surface
+        {:keys [world]} state]
+    (doseq [[y-index squares] (map-indexed vector world)
+            [x-index square]  (map-indexed vector squares)]
+      (draw-square surface (* x-index sq-width) (* y-index sq-height) square))))
+
+(defn deconstruct-environment
+  [world {:keys [x y]}]
+  (let [[above below]      (split-at y world)
+        [given-row below]  [(first below) (rest below)]
+        [left right]       (split-at x given-row)
+        [given-sq right]   [(first right) (rest right)]]
+    {:above above
+     :below below
+     :left left
+     :right right
+     :given-row given-row
+     :given-sq  given-sq}))
+
+(defn dig-away-earth
+  "Translate the given square coords into a tunnel square"
+  [world {:keys [x y]}]
+  (let [[above below]         (split-at y world)
+        [dig-row-then below]  [(first below) (rest below)]
+        [left right]          (split-at x dig-row-then)
+        [dig-square right]    [(first right) (rest right)]
+        dig-row-now           (apply str (concat left "t" right))]
+    (js/console.log "world:" (pr-str world) "x:" x "y:" y)
+    (js/console.log "above:" (pr-str above) "below:" (pr-str below))
+    (js/console.log "left:" (pr-str left) "dig-row-then:" (pr-str dig-row-then) "right:" (pr-str right) "dig-row-now:" (pr-str dig-row-now))
+    (concat above [dig-row-now] below)))
+
+(defn decide-where-to-dig
+  [world {:keys [x y] :as dig-coords}]
+  (if (or (= y 0) (= y 1)) ;; dig down into earth 2 squares first
+    {:x x :y (inc y)}
+    (let [{:keys [above below left right given-row given-sq]}
+          (deconstruct-environment world dig-coords)])))
+
+(defn move-paleontologist
+  [state]
+  (let [{:keys [world paleontologist]} state
+        new-paleontologist (decide-where-to-dig world paleontologist)
+        new-world (dig-away-earth world new-paleontologist)]
+    ;; start just digging down to get it working
+    (assoc state
+      :paleontologist new-paleontologist
+      :world new-world)))
 
 (defn game
   [timer state surface]
@@ -54,6 +128,7 @@
                    (update-canvas curr surface)
                    (-> curr
                        ;; Logic
+                       (move-paleontologist)
                        )))))
 
 (defn click
@@ -74,7 +149,7 @@
   (let [surface (surface)
         [_ width _] surface
         timer (goog.Timer. 500)
-        state (atom {})]
+        state (atom {:world initial-world :paleontologist {:x 1 :y 0}})]
     (update-canvas @state surface)
     (. timer (start))
     (events/listen timer goog.Timer/TICK #(game timer state surface))
