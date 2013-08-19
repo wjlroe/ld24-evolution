e20f3105edc4586cced8d8756e01948dff5b0260
William Roe <git@wjlr.org.uk>
2013-08-19 22:16:30 +0100

    Start doing async

---
+++ src/cljs/chaotic_god/core.cljs
@@ -0,0 +1,21 @@
+(ns chaotic-god.core
+  (:require [goog.dom :as dom]
+            [goog.Timer :as timer]
+            [goog.events :as events]
+            [goog.events.EventType :as event-type]
+            [goog.events.KeyCodes :as key-codes]
+            [chaotic-god.utils :as utils]
+            [cljs.core.async :refer [>! <! chan put! close! timeout]])
+  (:require-macros [cljs.core.async.macros :refer [go alt!]]))
+
+(defn ^:export main
+  []
+  (let [keyboard (utils/listen js/window :keydown)
+        mouse    (utils/listen js/window :mousedown)]
+    (go
+     (loop []
+       (let [[v sc] (alts! [keyboard mouse])]
+         (condp = sc
+           keyboard (.log js/console "keyboard pressed")
+           mouse (.log js/console "mouse clicked!")))
+       (recur)))))
---
+++ src/cljs/chaotic_god/utils.cljs
@@ -0,0 +1,29 @@
+(ns chaotic-god.utils
+  (:require [goog.dom :as dom]
+            [goog.Timer :as timer]
+            [goog.events :as events]
+            [goog.events.EventType :as event-type]
+            [goog.events.KeyCodes :as key-codes]
+            [cljs.core.async :refer [>! <! chan put! close! timeout]]))
+
+(def keyword->event-type
+  {:keyup goog.events.EventType.KEYUP
+   :keydown goog.events.EventType.KEYDOWN
+   :keypress goog.events.EventType.KEYPRESS
+   :click goog.events.EventType.CLICK
+   :dblclick goog.events.EventType.DBLCLICK
+   :mousedown goog.events.EventType.MOUSEDOWN
+   :mouseup goog.events.EventType.MOUSEUP
+   :mouseover goog.events.EventType.MOUSEOVER
+   :mouseout goog.events.EventType.MOUSEOUT
+   :mousemove goog.events.EventType.MOUSEMOVE
+   :focus goog.events.EventType.FOCUS
+   :blur goog.events.EventType.BLUR})
+
+(defn listen
+  ([el type] (listen el type nil))
+  ([el type f] (listen el type f (chan)))
+  ([el type f out]
+    (events/listen el (keyword->event-type type)
+      (fn [e] (when f (f e)) (put! out e)))
+    out))
