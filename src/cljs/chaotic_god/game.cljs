28b3b776b9b78862738e8630edb6a2ef0ba76610
William Roe <git@wjlr.org.uk>
2013-08-19 19:35:01 +0100

    Move vars around so they can reference each other
    Also there was a type = reserve/reverse

--- src/cljs/chaotic_god/game.cljs
+++ src/cljs/chaotic_god/game.cljs
@@ -51,6 +51,31 @@
    \s :surface
    \space :sky})
 
+(defn fill-rect
+  [[surface] [x y width height] [r g b]]
+  (set! (. surface -fillStyle) (str "rgb(" r "," g "," b ")"))
+  (.fillRect surface x y width height))
+
+(defn stroke-rect
+  [[surface] [x y width height] line-width [r g b]]
+  (set! (. surface -strokeStyle) (str "rgb(" r "," g "," b ")"))
+  (set! (. surface -lineWidth) line-width)
+  (.strokeRect surface x y width height))
+
+(defn fill-circle
+  [[surface] coords [r g b]]
+  (let [[x y d] coords]
+    (set! (. surface -fillStyle) (str "rgb(" r "," g "," b ")"))
+    (. surface (beginPath))
+    (.arc surface x y d 0 (* 2 Math/PI) true)
+    (. surface (closePath))
+    (. surface (fill))))
+
+(defn draw-square
+  [surface x y tile]
+  (fill-rect surface [x y sq-width sq-height] (tile tile-to-colour [0 0 0]))
+  (stroke-rect surface [x y sq-width sq-height] 1 [0 0 0]))
+
 (defn donothing [])
 
 (defn empty-event-handlers
@@ -108,30 +133,41 @@
      (. surface -width)
      (. surface -height)]))
 
-(defn fill-rect
-  [[surface] [x y width height] [r g b]]
-  (set! (. surface -fillStyle) (str "rgb(" r "," g "," b ")"))
-  (.fillRect surface x y width height))
+(defn edge-left?
+  [x y _]
+  (= x 1))
 
-(defn stroke-rect
-  [[surface] [x y width height] line-width [r g b]]
-  (set! (. surface -strokeStyle) (str "rgb(" r "," g "," b ")"))
-  (set! (. surface -lineWidth) line-width)
-  (.strokeRect surface x y width height))
+(defn edge-right?
+  [x y _]
+  (= x (- world-width 1)))
 
-(defn fill-circle
-  [[surface] coords [r g b]]
-  (let [[x y d] coords]
-    (set! (. surface -fillStyle) (str "rgb(" r "," g "," b ")"))
-    (. surface (beginPath))
-    (.arc surface x y d 0 (* 2 Math/PI) true)
-    (. surface (closePath))
-    (. surface (fill))))
+(defn tunnel-to-left?
+  [x y {:keys [left]}]
+  (tunnel? (last left)))
 
-(defn draw-square
-  [surface x y tile]
-  (fill-rect surface [x y sq-width sq-height] (tile tile-to-colour [0 0 0]))
-  (stroke-rect surface [x y sq-width sq-height] 1 [0 0 0]))
+(defn tunnel-to-right?
+  [x y {:keys [right]}]
+  (tunnel? (first right)))
+
+(defn tunnel-above?
+  [x y {:keys [above]}]
+  (tunnel? (nth (last above) x)))
+
+(defn tunnel-2-above?
+  [x y {:keys [above]}]
+  (tunnel? (nth (second (reverse above)) x)))
+
+(defn tunnel-below?
+  [x y {:keys [below]}]
+  (tunnel? (nth (first below) x)))
+
+(defn tunnel-depth
+  [x y {:keys [above]}]
+  (count (take-while tunnel? (reverse (map #(nth % x) above)))))
+
+(defn hit-bottom?
+  [x y {:keys [below]}]
+  (= 0 (count below)))
 
 (defn which-tunnel-tile
   [env {:keys [x y]}]
@@ -226,42 +262,6 @@
     (draw-paleontologist surface paleontologist)
     (when selection
       (stroke-rect surface [(* (:x selection) sq-width) (* (:y selection) sq-height) sq-width sq-height] 1 [43 197 0]))))
-
-(defn edge-left?
-  [x y _]
-  (= x 1))
-
-(defn edge-right?
-  [x y _]
-  (= x (- world-width 1)))
-
-(defn tunnel-to-left?
-  [x y {:keys [left]}]
-  (tunnel? (last left)))
-
-(defn tunnel-to-right?
-  [x y {:keys [right]}]
-  (tunnel? (first right)))
-
-(defn tunnel-above?
-  [x y {:keys [above]}]
-  (tunnel? (nth (last above) x)))
-
-(defn tunnel-2-above?
-  [x y {:keys [above]}]
-  (tunnel? (nth (second (reserve above)) x)))
-
-(defn tunnel-below?
-  [x y {:keys [below]}]
-  (tunnel? (nth (first below) x)))
-
-(defn tunnel-depth
-  [x y {:keys [above]}]
-  (count (take-while tunnel? (reverse (map #(nth % x) above)))))
-
-(defn hit-bottom?
-  [x y {:keys [below]}]
-  (= 0 (count below)))
 
 (defn decide-where-to-dig
   [world {:keys [x y] :as dig-coords}]
