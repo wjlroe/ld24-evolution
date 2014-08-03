14d2d22636194e28be5171efda3eeba936a87029
William Roe <willroe@gmail.com>
2014-08-03 12:30:16 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject chaotic-god "1.0.0-SNAPSHOT"
   :description "A game"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1450"]]
-  :plugins [[lein-cljsbuild "0.2.3"]]
-  :source-path "src/clj"
-  :cljsbuild {:builds [{
-                        :source-path "src/cljs"
-                        :compiler {
-                                   :output-to "resources/public/game.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]})
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2280"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 ]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-simpleton "1.3.0"]]
+  :source-paths ["src"]
+  :cljsbuild {:builds
+              [{:source-paths ["src/cljs"],
+                :compiler {:optimizations :whitespace
+                           :source-map "resources/public/game.js.map"
+                           :output-dir "resources/public"
+                           :output-to "resources/public/game.js",
+                           :pretty-print true}}]})
