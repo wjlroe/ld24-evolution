f0e753902effd024d2aa1ed811fbb1ef6b665664
William Roe <willroe@gmail.com>
2014-08-30 17:57:15 +0100

    Unused stuff

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
 (defproject chaotic-god "1.0.0-SNAPSHOT"
   :description "A game"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 ]
+                 [org.clojure/clojurescript "0.0-2280"]]
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [lein-simpleton "1.3.0"]]
-  :source-paths ["src"]
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"],
                 :compiler {:optimizations :whitespace
